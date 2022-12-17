bc5c785b94b30abab426febe25472d6dce09e675
Rolf <vcp-dauborn.de>
2022-12-17 19:09:46 +0100

    init

---
+++ setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+import os
+from setuptools import setup, find_packages
+
+def read_file(fname):
+    "Read a local file"
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='mkdocs-encriptmail-plugin',
+    version='0.9.3',
+    description='A MkDocs plugin that converts markdown encoded email-links into spambot save <a href> elements.',
+	long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown',
+    url='https://github.com/Rolfff/mkdocs-encriptmail-plugin',
+    author='Rolfff',
+	license='MIT',
+	python_requires='>=3.5',
+    install_requires=[
+		'mkdocs'
+	],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'encriptmail = src:encriptMailPlugin',
+        ]
+    }
+)
---
+++ tests/fixtures/docs/index.md
@@ -0,0 +1,21 @@
+# Test page
+
+Our image:
+
+![our image caption](github-octocat.png)
+
+## Our mail:
+
+- [Email-Link](test@test.test)
+
+- [test2@test.test](test2@test.test)
+
+- test3@test.test
+
+## Ein anderer Link:
+
+- [Test-Link](https://test.test)
+
+- [https://test.test](https://test.test)
+
+- https://test.test
